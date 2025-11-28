b22425e9df19e22bb5c555ca3d753875939d14a1
joonwan1024 <joonwan1024@naver.com>
2025-11-28 12:36:20 +0900

    feat: init project

---
+++ src/test/java/com/ssafy/yamyam_coach/YamyamCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.yamyam_coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamyamCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
